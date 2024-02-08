68982be595adfea982f03aba263a6402faeb70a1
79254 <nichiporenko-d@bk.ru>
2024-02-08 10:21:19 +0300

    salava club

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,21 @@
+﻿﻿#include <iostream>
+
+double calcul(double radius) {
+    return 3.14 * radius * radius;
+}
+double calculateRingArea(double outerR, double innerR) {
+    double outerArea = calcul(outerR);
+    double innerArea = calcul(innerR);
+    return outerArea - innerArea;
+}
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double outerR, innerR;
+    std::cout << "Введите внешний радиус кольца: ";
+    std::cin >> outerR;
+    std::cout << "Введите внутренний радиус кольца: ";
+    std::cin >> innerR;
+    double ringArea = calcul(outerR, innerR);
+    std::cout << "Площадь круга: " << ringArea << std::endl;
+    return 0;
+}
